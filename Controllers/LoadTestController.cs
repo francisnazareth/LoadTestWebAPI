aa87d3faad7f2895d26f9422fe163c0e825d727a
Francis Simy Nazareth <simy@outlook.com>
2022-02-22 14:17:36 +0300

    Add project files.

---
+++ Controllers/LoadTestController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LoadTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoadTestController : ControllerBase
+    {
+        private static int counter = 0;
+        private readonly String fileName = "userRegistration_";
+
+        // GET: api/<LoadTestController>
+        [HttpGet]
+        public string Get()
+        {
+            counter = (counter % 40) + 1;
+            string csvName = fileName + counter + ".csv";
+            Console.WriteLine("serving file name: " + csvName);
+            return csvName;
+        }
+
+        // GET api/<LoadTestController>/5
+        [HttpGet("{id}")]
+        public void Get(int id)
+        {
+            if( (id > 0) && (id <= 40) )
+                 counter = id - 1;
+            else counter = 0;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] User user)
+        {
+            Console.WriteLine( user.name +  "," +  user.email + "," + user.nationality);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,9 @@
+﻿namespace LoadTest
+{
+    public class User
+    {
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public string? nationality { get; set; }
+    }
+}
